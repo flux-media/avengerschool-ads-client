8adcf2255cc497af8756a67c561b7d447b296b59
Lee Han Kyeol <lee0hankyeol@gmail.com>
2016-05-24 14:48:14 +0900

    Disabled AJAX

--- avengerschool-ads-client.js
+++ avengerschool-ads-client.js
@@ -1,3 +1,68 @@
+function ASCourse (course) {
+	this.date = new Date(course.date);
+	// TODO: Why ten...?
+	this.date.setUTCHours(10);
+	this.title = course.title;
+	this.url = course.url;
+	this.thumbnail = course.thumbnail;
+	this.category = course.category;
+}
+ASCourse.prototype.isVisible = function() {
+	var MS_PER_HOUR = 1000 * 60 * 60, now = new Date();
+	return this.date.valueOf() - now.valueOf() > 2 * MS_PER_HOUR;
+}
+
+var courses = [];
+courses.push(new ASCourse({
+	title: '재무제표만 똑바로 봐도 기업이 보인다: 회계 정보의 올바른 이해',
+	url: 'https://avengerschool.com/product/understanding-accounting-data/',
+	thumbnail: 'https://avengerschool.com/wp-content/uploads/2016/04/title_%EC%9E%AC%EB%AC%B4%EC%A0%9C%ED%91%9C-300x200.jpg',
+	category: '경제',
+	date: '2016-05-24T19:30'}));
+courses.push(new ASCourse({
+	title: '총각네 야채가게에서 배우는 강남 고객 공략법',
+	url: 'https://avengerschool.com/product/how-to-approach-to-gangnam-consumers/',
+	thumbnail: 'https://avengerschool.com/wp-content/uploads/2016/05/%EC%9E%84%EC%B2%9C%EC%9D%BC-300x200.jpg',
+	category: '마케팅',
+	date: '2016-05-25T19:30'}));
+courses.push(new ASCourse({
+	title: '내가 조선의 카드뉴스 만드는 녀자다: 콘텐츠 마케팅의 전략 수립하기',
+	url: 'https://avengerschool.com/product/card-news-contents-marketing/',
+	thumbnail: 'https://avengerschool.com/wp-content/uploads/2016/04/title_%EC%B9%B4%EB%93%9C%EB%89%B4%EC%8A%A4-300x200.jpg',
+	category: '비즈니스',
+	date: '2016-05-26T19:30'}));
+courses.push(new ASCourse({
+	title: '예수가 한국 와서 고생이 많다: 대형교회가 말하지 않는 예수의 참된 가르침',
+	url: 'https://avengerschool.com/product/oh-poor-jesus/',
+	thumbnail: 'https://avengerschool.com/wp-content/uploads/2016/05/%EA%B9%80%EC%A7%84%ED%98%B8-300x200.jpg',
+	category: '헬조선',
+	date: '2016-05-28T14:00'}));
+courses.push(new ASCourse({
+	title: '직장인의 글쓰기: 효과적인 소통을 위한 비즈니스 글쓰기 비법',
+	url: 'https://avengerschool.com/product/writing-of-businessman/',
+	thumbnail: 'https://avengerschool.com/wp-content/uploads/2016/04/%EB%B0%B1%EC%8A%B9%EA%B6%8C5-300x200.jpg',
+	category: '글쓰기',
+	date: '2016-05-29T19:30'}));
+courses.push(new ASCourse({
+	title: '나 없이 채권투자 하지 마라: 금리와 채권투자의 올바른 이해',
+	url: 'https://avengerschool.com/product/invest-bonds/',
+	thumbnail: 'https://avengerschool.com/wp-content/uploads/2016/04/title_%EC%B1%84%EA%B6%8C%ED%88%AC%EC%9E%90-300x200.jpg',
+	category: '경제',
+	date: '2016-05-31T19:30'}));
+courses.push(new ASCourse({
+	title: '팟빵에서 직접 듣는 팟캐스트 마케팅 성공공식',
+	url: 'https://avengerschool.com/product/podbbang/',
+	thumbnail: 'https://avengerschool.com/wp-content/uploads/2016/05/%EA%B9%80%EB%8F%99%ED%9D%AC-300x200.jpg',
+	category: '마케팅',
+	date: '2016-06-01T19:30'}));
+courses.push(new ASCourse({
+	title: '기업의 레인메이커가 갖춰야 할 4대 역량: 내일부터 당장 차이를 만들어내기',
+	url: 'https://avengerschool.com/product/4-abilities-for-rainmakers/',
+	thumbnail: 'https://avengerschool.com/wp-content/uploads/2016/04/title_%EB%A0%88%EC%9D%B8%EB%A9%94%EC%9D%B4%EC%BB%A4-300x200.jpg',
+	category: '비즈니스',
+	date: '2016-06-02T19:30'}));
+
+
 (function ( $ ) {
 	function AvengerschoolAdsClientException(message) {
 		this.message = message;
@@ -10,6 +75,31 @@
 		return Math.floor(Math.random() * (max - min)) + min;
 	}
 
+	function getBanner(title, url, thumbnail, description) {
+		var onClick = "ga('send', 'event', 'Banner', 'click', 'Avengerschool');",
+			$bannerWrapper = $('<div/>', {
+				class: 'avengerschool-banner-wrapper'
+			});
+		$bannerWrapper.append($('<div/>', { class: 'header', text: title }));
+		$bannerWrapper.append(
+			$('<a/>', {
+				href: url,
+				target: '_blank',
+				html: $('<img>', { src: thumbnail }),
+				onClick: onClick
+			})
+		);
+		$bannerWrapper.append(
+			$('<a/>', {
+				href: url,
+				target: '_blank',
+				html: $('<div/>', { class: 'footer', text: description }),
+				onClick: onClick
+			})
+		);
+		return $bannerWrapper;
+	}
+
 	$.fn.appendAvengerschoolAds = function( options ) {
 		var settings = $.extend({
 			apiDataAction: 'get_ad_banner',
@@ -20,68 +110,110 @@
 
 		var $this = $(this),
 			$categoriesWrapper = $this.find(settings.categoriesWrapper);
+		if ($categoriesWrapper.length <= 0) {
+			return;
+		}
 
-		// Get categories as an array string.
+		// Find the proper paragraph with at least 120 characters, not having a <blockquote> parent.
+		var $entryContentPs = $this.find('.entry-content').find('p'),
+			indices = [];
+		$entryContentPs.each(function(index) {
+			var $this = $(this);
+			if ($this.text().length > 120 && $this.parent()[0].tagName != 'BLOCKQUOTE') {
+				indices.push(index);
+			}
+		});
+		var pIndex = getRandomInt(0, indices.length - 1);
+
+		// Match categories.
 		var $categoriesAnchors = $categoriesWrapper.find('a'),
-			categories = '[';
+			categorySlug = '', categories = [], coursesVisible = [];
 		$categoriesAnchors.each(function() {
-			categories += $(this).text() + ',';
+			var category = $(this).text();
+			switch (category) {
+				case '책':
+				case '학문':
+				case '인문':
+				case '역사':
+				case '영화':
+					categorySlug = '글쓰기';
+					break;
+
+				case '사회':
+				case '시사':
+					categorySlug = Math.random() < .5 ? '글쓰기': '헬조선';
+					break;
+
+				case '테크':
+				case '비즈니스':
+				case '인터뷰':
+					categorySlug = Math.random() < .5 ? '비즈니스': '마케팅';
+					break;
+
+				case '생활':
+					categorySlug = '비즈니스';
+					break;
+
+				case '경제':
+				case '투자':
+				case '국제':
+					categorySlug = '경제';
+					break;
+
+				case 'IT':
+				case '테크':
+					categorySlug = '마케팅';
+					break;
+
+				case '교육':
+				case '정치':
+					categorySlug = '헬조선';
+					break;
+
+				default:
+					var array = ['writing', 'business', 'economy', 'marketing', 'hell-chosun'];
+					categorySlug = array[Math.floor(Math.random() * array.length)];
+			}
 		});
-		categories = categories.slice(0, -1) + ']';
 
-		// Send categories this API server.
-		$.ajax({
-			data: {
-				action: settings.apiDataAction,
-				categories: categories
-			},
-			dataType: 'json',
-			method: settings.apiMethod,
-			success: function(response) {
-				var $bannerWrapper = $('<div/>', {
-					class: 'avengerschool-banner-wrapper'
-				});
-				$bannerWrapper.append(
-					$('<div/>', {
-						class: 'header',
-						text: response.data.title
-					})
-				);
-				$bannerWrapper.append(
-					$('<a/>', {
-						href: response.data.url,
-						target: '_blank',
-						html: $('<img>', {
-							src: response.data.thumbnail
-						})
-					})
-				);
-				$bannerWrapper.append(
-					$('<a/>', {
-						href: response.data.url,
-						target: '_blank',
-						html: $('<div/>', {
-							class: 'footer',
-							text: response.data.description
-						})
-					})
-				);
+		for (var i = 0, length = courses.length; i < length; i++) {
+			var course = courses[i];
+			if (course.category == categorySlug && course.isVisible()) {
+				coursesVisible.push(course);
+			}
+		}
 
-				// Find the proper paragraph with at least 120 characters, not having a <blockquote> parent.
-				var $entryContentPs = $this.find('.entry-content').find('p'),
-					indices = [];
-				$entryContentPs.each(function(index) {
-					var $this = $(this);
-					if ($this.text().length > 120 && $this.parent()[0].tagName != 'BLOCKQUOTE') {
-						indices.push(index);
-					}
-				});
-				var pIndex = getRandomInt(0, indices.length - 1);
+		if (coursesVisible.length > 0) {
+			var courseToShow = coursesVisible[Math.floor(Math.random() * coursesVisible.length)];
+			$entryContentPs.eq(indices[pIndex]).prepend(
+				getBanner('ㅍㅍㅅㅅ의 현업 전문가 실무 특강', courseToShow.url, courseToShow.thumbnail, courseToShow.title)
+			);
+		}
 
-				$entryContentPs.eq(indices[pIndex]).prepend($bannerWrapper);
-			},
-			url: settings.apiUrl
-		});
+		// TODO: Disabled for Avengerschool server performance.
+		// Send categories to API to get banner info.
+		// Get categories as an array string.
+		// var $categoriesAnchors = $categoriesWrapper.find('a'),
+		// 	categories = '[';
+		// $categoriesAnchors.each(function() {
+		// 	categories += $(this).text() + ',';
+		// });
+		// categories = categories.slice(0, -1) + ']';
+		// $.ajax({
+		// 	data: {
+		// 		action: settings.apiDataAction,
+		// 		categories: categories
+		// 	},
+		// 	dataType: 'json',
+		// 	method: settings.apiMethod,
+		// 	success: function(response) {
+		// 		var data = response.data,
+		// 			$bannerWrapper = getBanner(data.title, data.url,
+		// 				data.thumbnail, data.description);
+		// 		$entryContentPs.eq(indices[pIndex]).prepend($bannerWrapper);
+		// 	},
+		// 	url: settings.apiUrl
+		// });
 	};
 
 	$(document).ready(function() {
@@ -90,11 +222,6 @@
 		// After being scrolled.
 		$(window).on('ppss_infinity_loaded', function(event, url, targetId) {
 			$('#' + targetId).appendAvengerschoolAds();
-		});
-
-		// Track the events.
-		$('body').on('click', '.avengerschool-banner-wrapper a', function(e) {
-			ga('send', 'event', 'Banner', 'click', 'Avengerschool');
 		});
 	});
 }( jQuery ));
